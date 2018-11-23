f5a1478f97f462c75eac4c8a19f2de5761087d6d
samsonjj <samsonjj@vcu.edu>
2018-11-23 15:00:51 -0500

    steps A-F completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,28 +2,35 @@ package com.company;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 
     static final String INPUT_FILE_NAME = "sentences.txt";
+    static final String STOP_WORDS_FILE_NAME = "stop_words.txt";
 
     public static void main(String[] args) {
 
         System.out.println("Starting...");
 
         ArrayList<String> sentences = new ArrayList<>();
+        HashSet<String> stopWords = new HashSet<>();
 
         // Read in sentences
-        try(Scanner fileScanner = new Scanner(new File(INPUT_FILE_NAME))) {
-            while(fileScanner.hasNextLine()) {
-                sentences.add(fileScanner.nextLine());
+        try(Scanner sentenceScanner = new Scanner(new File(INPUT_FILE_NAME)); Scanner stopScanner = new Scanner(new File(STOP_WORDS_FILE_NAME))) {
+            while(sentenceScanner.hasNextLine()) {
+                sentences.add(sentenceScanner.nextLine());
+            }
+            while(stopScanner.hasNext()) {
+                stopWords.add(stopScanner.next());
             }
         } catch(Exception e) {
             System.out.println("error reading file \"" + INPUT_FILE_NAME + "\"");
         }
 
-        ArrayList<String[]> sentence_tokens = new ArrayList<>();
+        ArrayList<ArrayList<String>> sentence_tokens = new ArrayList<>();
 
         // Remove punctuation, special characters, numbers
         for(int i = 0; i < sentences.size(); i++) {
@@ -35,24 +42,37 @@ public class Main {
 
         // Split by spaces
         for(int i = 0; i < sentences.size(); i++) {
-            sentence_tokens.add(sentences.get(i).split(" +"));
+            sentence_tokens.add(new ArrayList<String>(Arrays.asList(sentences.get(i).split(" +"))));
+        }
+
+        // Remove stop words
+        for(ArrayList<String> tokens : sentence_tokens) {
+            int currentIndex = 0;
+            while(currentIndex < tokens.size()) {
+                if(stopWords.contains(tokens.get(currentIndex))) {
+                    tokens.remove(currentIndex);
+                }
+                else {
+                    currentIndex++;
+                }
+            }
         }
 
         // Stem each token
         for(int i = 0; i < sentence_tokens.size(); i++) {
-            for(int j = 0; j < sentence_tokens.get(i).length; j++) {
-                String token = sentence_tokens.get(i)[j];
+            for(int j = 0; j < sentence_tokens.get(i).size(); j++) {
+                String token = sentence_tokens.get(i).get(j);
                 Stemmer s = new Stemmer();
                 for(int k = 0; k < token.length(); k++) {
                     s.add(token.charAt(k));
                 }
                 s.stem();
                 String result = s.toString();
-                sentence_tokens.get(i)[j] = result;
+                sentence_tokens.get(i).set(j, result);
             }
         }
 
-        for(String[] tokens : sentence_tokens) {
+        for(ArrayList<String> tokens : sentence_tokens) {
             printlnTokenArray(tokens);
         }
 
@@ -61,10 +81,14 @@ public class Main {
 
     }
 
-    public static void printlnTokenArray(String[] tokens) {
-        for(int i = 0; i < tokens.length; i++) {
-            if(i == 0) System.out.print(tokens[i]);
-            else System.out.print(" " + tokens[i]);
+    public static void printlnTokenArray(Iterable<String> tokens) {
+        boolean first = true;
+        for(String token : tokens) {
+            if(first) {
+                System.out.print(token);
+                first = false;
+            }
+            else System.out.print(" " + token);
         }
         System.out.println();
     }
